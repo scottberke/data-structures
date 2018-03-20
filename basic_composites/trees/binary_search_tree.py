939fbf5280174edd3eb7f01c09299b9f518585f2
Scott B <scottberke@gmail.com>
2018-03-20 14:41:18 -0600

    Add docstring for BST

--- basic_composites/trees/binary_search_tree.py
+++ basic_composites/trees/binary_search_tree.py
@@ -2,9 +2,21 @@ from tree import *
 
 class BinarySearchTree():
     def __init__(self, root=None):
+        """
+        Input:
+            root (node) = Root node of the tree
+        Output:
+            BinarySearchTree
+        """
         self.__root = root
 
     def insert_node(self, current_node, node):
+        """
+        Input:
+            current_node (node) = Node to insert after
+            node (node) = Node being inserted
+        Output:
+        """
         if node.data < current_node.data:
             if current_node.left:
                 self.insert_node(current_node.left, node)
@@ -17,6 +29,11 @@ class BinarySearchTree():
                 current_node.right = node
 
     def insert(self, data):
+        """
+        Input:
+            data (Node) = Node to be inserted into the tree
+        Output:
+        """
         node = self.handle_data(data)
         if not self.__root:
             self.__root = node
@@ -24,9 +41,21 @@ class BinarySearchTree():
             self.insert_node(self.__root, node)
 
     def find(self, data):
+        """
+        Input:
+            data (Node) = Node to find in the tree
+        Output:
+            Node = Node being retrieved
+        """
         return self.find_node(self.__root, data)
 
     def find_node(self, current_node, data):
+        """
+        Input:
+            current_node (Node) = Node currently serving as root of tree or subtree
+            data (Node) = Node to find in the tree
+        Output:
+        """
         if current_node.data == data:
             return current_node.data
 
